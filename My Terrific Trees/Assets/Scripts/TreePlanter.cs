937f7447c17ac0ca2ca31835a1fdf372d70c04fc
Anthony <awessel@mail.bradley.edu>
2020-11-04 15:37:46 -0600

    Rewrote TreePlanter.cs to work on the new grid system
    Does not currently check if a tree has already been planted at a specific position

--- My Terrific Trees/Assets/Scripts/TreePlanter.cs
+++ My Terrific Trees/Assets/Scripts/TreePlanter.cs
@@ -12,13 +12,12 @@ public class TreePlanter : MonoBehaviour
     public int remainingTrees = 5;
     public Text treeCountText;
 
-    GameBoard board;
+    public Vector2Int boardSize;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        board = FindObjectOfType<GameBoard>();
         if (treePrefab == null) Debug.LogWarning("No tree prefab set. Will throw error if TreePlanter tries to plant a tree.");
         if (treeCountText == null) Debug.LogWarning("No tree count text is set. There is no UI keeping track of remaining trees.");
     }
@@ -34,7 +33,7 @@ public class TreePlanter : MonoBehaviour
             if (remainingTrees > 0)
             {
                 PlantTreeOnTile(mouseTile);
-                Debug.Log("mouseTile Coords at plant: " + mouseTile);
+                //Debug.Log("mouseTile Coords at plant: " + mouseTile);
             }
             else
                 Debug.Log("You are out of trees!");
@@ -47,22 +46,14 @@ public class TreePlanter : MonoBehaviour
     public void PlantTreeOnTile(Vector2Int tileCoordinates)
     {
         /// Player clicked off of the grid, don't try to plant a tree
-        if (tileCoordinates.x < -board.size.x / 2 || tileCoordinates.x > board.size.x / 2) return;
-        if (tileCoordinates.y < -board.size.y / 2 || tileCoordinates.y > board.size.y / 2) return;
+        if (tileCoordinates.x < -boardSize.x / 2 || tileCoordinates.x > boardSize.x / 2) return;
+        if (tileCoordinates.y < -boardSize.y / 2 || tileCoordinates.y > boardSize.y / 2) return;
 
-        /// Adjust the coordinates to be completely positive (so they can be passed into the tile array)
-        tileCoordinates.x += board.size.x / 2;
-        tileCoordinates.y += board.size.y / 2;
-        int tileX = board.size.x - tileCoordinates.x-1;
-        int tileY = board.size.y - tileCoordinates.y-1;
-
-        /// Get the tile
-        Transform tile = board.tileArray[tileX + board.size.x* tileY].transform;
-
-        /// Plant the tree
-        if (tile.childCount < 2)
+        /// Check if tree is already planted at this spot
+        if (true) 
         {
-            Instantiate(treePrefab, tile);
+            /// Plant the tree
+            Instantiate(treePrefab, new Vector3(tileCoordinates.x, 0, tileCoordinates.y), treePrefab.transform.rotation);
             remainingTrees--;
         }
         
