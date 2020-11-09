a27722b0cae76ab77cdf36b0a585a674e4d3667d
lbarrettCIS497 <lbarrett@mail.bradley.edu>
2020-11-09 14:35:49 -0600

    Saplings now grow into small trees, which grow into big trees

---
+++ My Terrific Trees/Assets/Scripts/TreeGrower.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeGrower : MonoBehaviour
+{
+    public GameObject smallTreePrefab;
+    public GameObject bigTreePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("GrowTree");
+    }
+
+    IEnumerator GrowTree()
+    {
+        yield return new WaitForSeconds(10);
+
+        if (gameObject.CompareTag("Sapling"))
+        {
+            Instantiate(smallTreePrefab, transform.position, transform.rotation);
+        }
+        else if (gameObject.CompareTag("Small Tree"))
+        {
+            Instantiate(bigTreePrefab, transform.position, transform.rotation);
+        }
+        Destroy(gameObject);
+    }
+
+}
