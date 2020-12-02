d8addb372c6124e2cbd9f49922cd5e6cf244c0b4
Wolfgang Gross <60198468+WolfgangPGross@users.noreply.github.com>
2020-12-02 15:02:41 -0600

    Added SawBlades

---
+++ My Terrific Trees/Assets/Scripts/Woodcutter/Model Scripts/SpinBlades.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinBlades : MonoBehaviour
+{
+    public GameObject sawBladeUpper;
+    public GameObject sawBladeLower;
+
+    public float spinSpeedUpper = 8f;
+    public float spinSpeedLower = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sawBladeLower.transform.Rotate(0f, -spinSpeedLower,0f);
+        sawBladeUpper.transform.Rotate(0f, spinSpeedUpper, 0f);
+    }
+}
